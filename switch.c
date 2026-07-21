f9d374318c704bb26bf4b73bba33ea5134b9aaa8
Abiprinci <abiprinciya@gmail.com>
2026-07-21 20:32:55 +0530

    Added switch case problem

---
+++ switch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, choice;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("\nCalculator Menu\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            printf("Result = %d\n", num1 + num2);
+            break;
+
+        case 2:
+            printf("Result = %d\n", num1 - num2);
+            break;
+
+        case 3:
+            printf("Result = %d\n", num1 * num2);
+            break;
+
+        case 4:
+            if (num2 != 0)
+                printf("Result = %.2f\n", (float)num1 / num2);
+            else
+                printf("Division by zero is not possible.\n");
+            break;
+
+        default:
+            printf("Invalid Choice!\n");
+    }
+
+    return 0;
+}
